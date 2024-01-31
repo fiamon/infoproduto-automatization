e44d87461f8320b5bea998ce944a7e275c3b8b8d
fiamon <juliogustavofi@gmail.com>
2024-01-30 21:18:20 -0300

    feat: add support to https request/responses

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
 import app from './app';
+import fs from 'node:fs';
+import https from 'node:https';
 import { env } from './env';
+import { Request, Response } from 'express';
 
-app.listen(env.PORT, (): void => {
-    console.log(`🚀 Server running on port ${env.PORT}`);
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello from express server.');
 });
+
+https
+    .createServer(
+        {
+            key: fs.readFileSync('key.pem'),
+            cert: fs.readFileSync('cert.pem'),
+        },
+        app,
+    )
+    .listen(env.PORT, () => {
+        console.log(`🚀 Server running on port ${env.PORT}`);
+    });
