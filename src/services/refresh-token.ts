31079f971570a48e61e1ac27b8347fa3540b8491
fiamon <juliogustavofi@gmail.com>
2024-02-10 19:37:48 -0300

    fix: refresh token function

--- src/services/refresh-token.ts
+++ src/services/refresh-token.ts
@@ -1,35 +1,12 @@
 import axios, { AxiosResponse } from 'axios';
 import { env } from '../env';
 import { PrismaClient } from '@prisma/client';
-import { GetAuthorizationCodeResponse, RefreshToken } from '../@types';
+import { RefreshToken } from '../@types';
 
 const prisma = new PrismaClient();
 
-export async function getAuthorizationCode(userCode: string): Promise<GetAuthorizationCodeResponse> {
-    const authorizationCode: AxiosResponse<GetAuthorizationCodeResponse> = await axios.post(
-        'https://api.mercadolibre.com/oauth/token',
-        {
-            grant_type: 'authorization_code',
-            client_id: env.MERCADO_LIVRE_APP_ID,
-            client_secret: env.MERCADO_LIVRE_SECRET_KEY,
-            code: userCode,
-            redirect_uri: 'https://65b5-2804-30c-b2a-3100-30ee-4655-b78b-8aa6.ngrok-free.app',
-            code_verifier: '$CODE_VERIFIER',
-        },
-    );
-
-    await prisma.tokens.create({
-        data: {
-            access_token: authorizationCode.data.access_token,
-            refresh_token: authorizationCode.data.refresh_token,
-        },
-    });
-
-    return authorizationCode.data;
-}
-
 export async function updateMercadoLivreRefreshToken() {
-    const refreshToken = prisma.tokens.findMany({
+    const getTheLastRegister = await prisma.tokens.findMany({
         orderBy: {
             created_at: 'desc',
         },
@@ -40,7 +17,7 @@ export async function updateMercadoLivreRefreshToken() {
         grant_type: 'refresh_token',
         client_id: env.MERCADO_LIVRE_APP_ID,
         client_secret: env.MERCADO_LIVRE_SECRET_KEY,
-        refresh_token: refreshToken,
+        refresh_token: getTheLastRegister[0].refresh_token,
     });
 
     await prisma.tokens.create({
@@ -52,3 +29,5 @@ export async function updateMercadoLivreRefreshToken() {
 
     return token;
 }
+
+updateMercadoLivreRefreshToken();
